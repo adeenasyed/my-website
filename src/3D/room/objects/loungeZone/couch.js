bc93435e00f661f62f7b1ab1e87fe2429b5c2a01
adeenasyed <adeenasyed@icloud.com>
2026-09-08 23:37:06 -0400

    couch

--- src/3D/room/objects/loungeZone/couch.js
+++ src/3D/room/objects/loungeZone/couch.js
@@ -4,7 +4,7 @@ import { ROOM_WIDTH } from '../../constants.js'
 export function loadCouch() {
   return loadObject('/objects/couch.glb', {
     size: 410,
-    rotation: { y: Math.PI / 2 },
+    rotation: { y: -Math.PI / 2 },
     position: (box) => ({
       x: -ROOM_WIDTH / 2 - box.min.x,
       y: -box.min.y,
